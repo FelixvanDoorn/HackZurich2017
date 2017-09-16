ab4d7518da3409274a2db203f7b1fc578899de32
Thomas Verelst <thomas.verelst@hotmail.com>
2017-09-16 18:49:51 +0200

    better joystick

--- interface/app/static/amazingstuff.js
+++ interface/app/static/amazingstuff.js
@@ -1,18 +1,34 @@
 
-var sendDriveData = function(manager)
+var sendDriveData = function(joystick)
 {
 
-    nipple = manager.get(0);
-    console.log("ids"+manager.ids)
-    if(nipple == undefined || nipple == false || nipple == null ){
-        var xpos = 0;
-        var ypos = 0;
-    }else{
-        var xpos = -nipple.frontPosition.x*2/nipple.options.size
-        var ypos = -nipple.frontPosition.y*2/nipple.options.size
+    // nipple = manager.get(0);
+    // console.log("ids"+manager.ids)
+    // if(nipple == undefined || nipple == false || nipple == null ){
+    //     var xpos = 0;
+    //     var ypos = 0;
+    // }else{
+    //     var xpos = -nipple.frontPosition.x*2/nipple.options.size
+    //     var ypos = -nipple.frontPosition.y*2/nipple.options.size
+    // }
+
+    var posx = joystick.deltaX()/5
+    if(posx > 1){
+        posx = 1
     }
-    console.log("LOGLOG"+nipple+ " " + xpos + " " + ypos )
-    $.post('/dataDrive', {throttle: ypos, angle : xpos}, function() {
+    if(posx < -1){
+        posx = -1
+    }
+    var posy = joystick.deltaY()/5
+    if(posy > 1){
+        posy = 1
+    }
+    if(posy < -1){
+        posy = -1
+    }
+
+    console.log("LOGLOG"+joystick+ " " + posx + " " + posy)
+    $.post('/dataDrive', {throttle: 0, angle : 0}, function() {
         console.log("dataDrive done")
     })
 
@@ -20,18 +36,21 @@ var sendDriveData = function(manager)
 
 $(document).ready(function(){
    
+    //var y = document.getElementsByClassName('container');
     
-    var options = {
-        color : "Blue",          // distance to recycle previous joystick in
-        fadeTime : 0,
-        mode: 'dynamic',
-        position: {left: '50%', top: '50%'}
-    };
+    // var options = {
+    //   //  zone: y[0],
+    //     color : "Blue",          // distance to recycle previous joystick in
+    //     fadeTime : 10,
+    //     mode: 'dynamic'
+    // };
    
-    var manager = nipplejs.create(options);
+    // var manager = nipplejs.create(options);
+
+    var joystick = new VirtualJoystick()
     var timeoutID = window.setInterval(function(){
         
-        sendDriveData(manager)
+        sendDriveData(joystick)
     }, 200);
     
 })
